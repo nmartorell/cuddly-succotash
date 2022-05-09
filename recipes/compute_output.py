8952bb8cf4a433b90d92b18dac1248dfa6e292d1
nmartorell <me@nedmartorell.com>
2022-05-09 21:53:53 +0000

    Saved recipe: DATASTUFFMASTER.compute_output

--- recipes/compute_output.py
+++ recipes/compute_output.py
@@ -7,14 +7,8 @@ from dataiku import pandasutils as pdu
 country_economic_indicators = dataiku.Dataset("country_economic_indicators")
 country_economic_indicators_df = country_economic_indicators.get_dataframe()
 
-
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
-output_df = country_economic_indicators_df # For this sample code, simply copy input to output
-
+# MY CHANGES!!!
 
 # Write recipe outputs
 output = dataiku.Dataset("output")
-output.write_with_schema(output_df)
+output.write_with_schema(country_economic_indicators_df)
